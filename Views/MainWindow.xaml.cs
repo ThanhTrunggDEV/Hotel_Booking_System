e04c615aee8d377be3fc043e9b27e1239a1c92bf
Nguyễn Thành Trung <trungff07@gmail.com>
2025-08-04 22:41:35 +0700

    create some directories for MVVM structure

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hotel_Manager
+namespace Hotel_Manager.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
