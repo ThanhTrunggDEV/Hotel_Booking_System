d0c4ed21dfe994f30f82cbc369968c32c6943be5
Nguyễn Thành Trung <trungff07@gmail.com>
2025-08-04 23:04:51 +0700

    Create some domain models

--- DomainModels/Invoice.cs
+++ DomainModels/Invoice.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Hotel_Manager.DomainModels
 {
-    internal class Invoce
+    internal class Invoice
     {
     }
 }
--- DomainModels/User.cs
+++ DomainModels/User.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Hotel_Manager.DomainModels
 {
-    internal class Users
+    internal class User
     {
+        public string UserID { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
