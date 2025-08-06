a066d550aa71c47025ac13e9b906fa9a5cf8fc51
Nguyễn Thành Trung <trungff07@gmail.com>
2025-08-06 11:03:16 +0700

    config the database

---
+++ Services/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Hotel_Manager.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Manager.Services
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=data.dat");
+        }
+    }
+}
