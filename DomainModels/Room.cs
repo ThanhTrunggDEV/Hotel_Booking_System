3e4ad6335c9aea5892df0cab4b8313286e2aa647
Nguyễn Thành Trung <trungff07@gmail.com>
2025-08-09 12:32:55 +0700

    add image for rooms

--- DomainModels/Room.cs
+++ DomainModels/Room.cs
@@ -11,6 +11,7 @@ namespace Hotel_Manager.DomainModels
         public string RoomID { get; set; } = "";
         public string RoomNumber { get; set; } = "";
         public string HotelID { get; set; } = "";
+        public string RoomImage { get; set; } = "";
         public string RoomTypeID { get; set; } = "";
         public bool IsAvailable { get; set; }
     }
