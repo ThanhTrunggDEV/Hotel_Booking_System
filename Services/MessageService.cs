e925c8b8f3cb0e6193d3cb3b135e2b2640e9ee7f
Nguyễn Thành Trung <trungff07@gmail.com>
2025-08-26 23:34:04 +0700

    add message service to pass the useremail to other window

---
+++ Services/MessageService.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Booking_System.Services
+{
+    class MessageService : ValueChangedMessage<string>
+    {   
+        public MessageService(string value) : base(value) { }
+    }
+}
