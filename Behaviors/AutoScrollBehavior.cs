727537a78f09e1e755416346dec59614d773250d
Nguyễn Thành Trung <trungff07@gmail.com>
2025-09-02 20:49:03 +0700

    add auto  scroll down behavior for chat panel

---
+++ Behaviors/AutoScrollBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Hotel_Booking_System.Behaviors
+{
+    public static class ListBoxExtensions
+    {
+        public static readonly DependencyProperty AutoScrollProperty =
+            DependencyProperty.RegisterAttached(
+                "AutoScroll",
+                typeof(bool),
+                typeof(ListBoxExtensions),
+                new PropertyMetadata(false, OnAutoScrollChanged));
+
+        public static bool GetAutoScroll(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoScrollProperty);
+        }
+
+        public static void SetAutoScroll(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoScrollProperty, value);
+        }
+
+        private static void OnAutoScrollChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ListBox listBox)
+            {
+                if ((bool)e.NewValue)
+                {
+                    if (listBox.ItemsSource is INotifyCollectionChanged collection)
+                    {
+                        collection.CollectionChanged += (s, ev) =>
+                        {
+                            if (listBox.Items.Count > 0)
+                            {
+                                listBox.ScrollIntoView(listBox.Items[listBox.Items.Count - 1]);
+                            }
+                        };
+                    }
+                }
+            }
+        }
+    }
+}
