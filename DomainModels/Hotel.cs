f150db34dc4b7a0215c31295f62ab1b9295a23d5
Nguyễn Thành Trung <trungff07@gmail.com>
2025-09-05 13:34:03 +0700

    remove hotel type from hotel model because of conflict

--- DomainModels/Hotel.cs
+++ DomainModels/Hotel.cs
@@ -14,7 +14,6 @@ namespace Hotel_Booking_System.DomainModels
         public string Address { get; set; }
         public string City { get; set; }
         public string HotelImage { get; set; }
-        public string HotelType { get; set; }
         public string Description { get; set; }
         public int Rating { get; set; }
     }
