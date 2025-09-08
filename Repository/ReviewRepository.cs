e8eaaaebe1ac4c4b0f2f70df722ed26498877cc1
Nguyễn Thành Trung <trungff07@gmail.com>
2025-09-08 18:06:46 +0700

    implement review repo

---
+++ Repository/ReviewRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel_Booking_System.DomainModels;
+using Hotel_Booking_System.Interfaces;
+using Hotel_Booking_System.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Booking_System.Repository
+{
+    internal class ReviewRepository : IReviewRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ReviewRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Review data)
+        {
+            await _context.Reviews.AddAsync(data);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+            if (review != null)
+            {
+                _context.Reviews.Remove(review);
+            }
+        }
+
+        public async Task<List<Review>> GetAllAsync()
+        {
+            return await _context.Reviews.ToListAsync();
+        }
+
+        public async Task<Review?> GetByIdAsync(string id)
+        {
+            return await _context.Reviews.FindAsync(id);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Review data)
+        {
+            _context.Reviews.Update(data);
+            await SaveAsync();
+        }
+    }
+}
