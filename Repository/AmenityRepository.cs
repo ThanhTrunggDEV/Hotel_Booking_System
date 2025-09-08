5a6ed1a1206c9bb074fbf57280609273722e34ac
Nguyễn Thành Trung <trungff07@gmail.com>
2025-09-08 17:50:21 +0700

    implement Amenity repo

---
+++ Repository/AmenityRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel_Booking_System.DomainModels;
+using Hotel_Booking_System.Interfaces;
+using Hotel_Booking_System.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Booking_System.Repository
+{
+    class AmenityRepository : IAmenityRepository
+    {
+        private readonly AppDbContext _context;
+        public AmenityRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Amenity data)
+        {
+            await _context.Amenities.AddAsync(data);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+           var data = await _context.Amenities.FindAsync(id);
+            if (data != null)
+                _context.Amenities.Remove(data);
+        }
+
+        public Task<List<Amenity>> GetAllAsync() => _context.Amenities.ToListAsync();
+
+        public Task<Amenity?> GetByIdAsync(string id) => _context.Amenities.FirstOrDefaultAsync(r => r.AmenityID == id);
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Amenity data)
+        {
+            _context.Amenities.Update(data);
+            await SaveAsync();
+        }
+    }
+}
