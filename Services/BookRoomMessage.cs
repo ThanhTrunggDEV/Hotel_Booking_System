e40255d9e821aa07ff3fae8a409030bc84bf9b26
Nguyễn Thành Trung <trungff07@gmail.com>
2025-09-09 08:24:18 +0700

    add book room message

---
+++ Services/BookRoomMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using Hotel_Booking_System.DomainModels;
+
+namespace Hotel_Booking_System.Services
+{
+    public class BookRoomMessage : ValueChangedMessage<(Room Room, User User)>
+    {
+        public BookRoomMessage(Room room, User user) : base((room, user)) { }
+    }
+}
