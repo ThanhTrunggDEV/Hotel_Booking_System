e6cfdebd16a5815edce2e0767bd941810f8283d8
Nguyễn Thành Trung <140838446+ThanhTrunggDEV@users.noreply.github.com>
2025-09-13 08:16:41 +0700

    Inject SuperAdmin view model and configure tab contexts

--- Interfaces/ISuperAdminViewModel.cs
+++ Interfaces/ISuperAdminViewModel.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Hotel_Booking_System.Interfaces
 {
     interface ISuperAdminViewModel
     {
+        Task LoadDataAsync();
     }
 }
+
