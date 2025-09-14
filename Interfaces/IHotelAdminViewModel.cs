7ab6139b02dd9a3f4c146bd4c8e024d203764f56
Nguyễn Thành Trung <140838446+ThanhTrunggDEV@users.noreply.github.com>
2025-09-14 17:34:37 +0700

    Remove explicit command props from hotel admin interface

--- Interfaces/IHotelAdminViewModel.cs
+++ Interfaces/IHotelAdminViewModel.cs
@@ -1,6 +1,5 @@
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.Input;
 using Hotel_Booking_System.DomainModels;
 
 namespace Hotel_Booking_System.Interfaces
@@ -15,14 +14,6 @@ namespace Hotel_Booking_System.Interfaces
         ObservableCollection<Booking> Bookings { get; }
         ObservableCollection<Review> Reviews { get; }
         User CurrentUser { get; set; }
-
-        IRelayCommand NewHotelCommand { get; }
-        IAsyncRelayCommand UpdateHotelInfoCommand { get; }
-        IAsyncRelayCommand AddRoomCommand { get; }
-        IAsyncRelayCommand<Room?> EditRoomCommand { get; }
-        IAsyncRelayCommand<Room?> RemoveRoomCommand { get; }
-        IAsyncRelayCommand<Booking?> ConfirmBookingCommand { get; }
-        IAsyncRelayCommand<Booking?> CancelBookingCommand { get; }
 
         Task LoadReviewsAsync();
     }
