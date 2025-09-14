4a787b69b56cb4ca4fb805d777cef957ba075a13
Nguyễn Thành Trung <140838446+ThanhTrunggDEV@users.noreply.github.com>
2025-09-14 17:29:54 +0700

    Use RelayCommand interfaces for hotel admin commands

--- Interfaces/IHotelAdminViewModel.cs
+++ Interfaces/IHotelAdminViewModel.cs
@@ -1,6 +1,6 @@
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
-using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 using Hotel_Booking_System.DomainModels;
 
 namespace Hotel_Booking_System.Interfaces
@@ -16,13 +16,13 @@ namespace Hotel_Booking_System.Interfaces
         ObservableCollection<Review> Reviews { get; }
         User CurrentUser { get; set; }
 
-        ICommand NewHotelCommand { get; }
-        ICommand UpdateHotelInfoCommand { get; }
-        ICommand AddRoomCommand { get; }
-        ICommand EditRoomCommand { get; }
-        ICommand RemoveRoomCommand { get; }
-        ICommand ConfirmBookingCommand { get; }
-        ICommand CancelBookingCommand { get; }
+        IRelayCommand NewHotelCommand { get; }
+        IAsyncRelayCommand UpdateHotelInfoCommand { get; }
+        IAsyncRelayCommand AddRoomCommand { get; }
+        IAsyncRelayCommand<Room?> EditRoomCommand { get; }
+        IAsyncRelayCommand<Room?> RemoveRoomCommand { get; }
+        IAsyncRelayCommand<Booking?> ConfirmBookingCommand { get; }
+        IAsyncRelayCommand<Booking?> CancelBookingCommand { get; }
 
         Task LoadReviewsAsync();
     }
