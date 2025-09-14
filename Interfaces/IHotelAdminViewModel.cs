1f6871fe141d6bee17ca37af3583cd0709a7a2f1
Nguyễn Thành Trung <140838446+ThanhTrunggDEV@users.noreply.github.com>
2025-09-14 17:23:11 +0700

    Expose hotel admin commands

--- Interfaces/IHotelAdminViewModel.cs
+++ Interfaces/IHotelAdminViewModel.cs
@@ -1,12 +1,13 @@
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Hotel_Booking_System.DomainModels;
 
 namespace Hotel_Booking_System.Interfaces
 {
 
        
-    interface IHotelAdminViewModel
+    public interface IHotelAdminViewModel
     {
         ObservableCollection<Hotel> Hotels { get; }
         Hotel? CurrentHotel { get; set; }
@@ -14,6 +15,15 @@ namespace Hotel_Booking_System.Interfaces
         ObservableCollection<Booking> Bookings { get; }
         ObservableCollection<Review> Reviews { get; }
         User CurrentUser { get; set; }
+
+        ICommand NewHotelCommand { get; }
+        ICommand UpdateHotelInfoCommand { get; }
+        ICommand AddRoomCommand { get; }
+        ICommand EditRoomCommand { get; }
+        ICommand RemoveRoomCommand { get; }
+        ICommand ConfirmBookingCommand { get; }
+        ICommand CancelBookingCommand { get; }
+
         Task LoadReviewsAsync();
     }
 }
