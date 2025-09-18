2612b74420d87b86146451cf171e4667d7967aca
Nguyễn Thành Trung <140838446+ThanhTrunggDEV@users.noreply.github.com>
2025-09-18 09:08:05 +0700

    Ensure review replies update UI and user reviews scroll

--- DomainModels/Review.cs
+++ DomainModels/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Hotel_Booking_System.DomainModels
 {
-    public class Review
+    public class Review : INotifyPropertyChanged
     {
         [Key]
         public string ReviewID { get; set; }
@@ -20,10 +21,78 @@ namespace Hotel_Booking_System.DomainModels
         public string Comment { get; set; }
         public DateTime CreatedAt { get; set; }
 
-        [NotMapped]
-        public string ReviewerName { get; set; } = string.Empty;
+        private string? _adminReply;
+        public string? AdminReply
+        {
+            get => _adminReply;
+            set
+            {
+                if (_adminReply == value)
+                {
+                    return;
+                }
+
+                _adminReply = value;
+                OnPropertyChanged(nameof(AdminReply));
+            }
+        }
 
         [NotMapped]
-        public string ReviewerAvatarUrl { get; set; } = string.Empty;
+        private string _reviewerName = string.Empty;
+        public string ReviewerName
+        {
+            get => _reviewerName;
+            set
+            {
+                if (_reviewerName == value)
+                {
+                    return;
+                }
+
+                _reviewerName = value;
+                OnPropertyChanged(nameof(ReviewerName));
+            }
+        }
+
+        [NotMapped]
+        private string _reviewerAvatarUrl = string.Empty;
+        public string ReviewerAvatarUrl
+        {
+            get => _reviewerAvatarUrl;
+            set
+            {
+                if (_reviewerAvatarUrl == value)
+                {
+                    return;
+                }
+
+                _reviewerAvatarUrl = value;
+                OnPropertyChanged(nameof(ReviewerAvatarUrl));
+            }
+        }
+
+        [NotMapped]
+        private string _adminReplyDraft = string.Empty;
+        public string AdminReplyDraft
+        {
+            get => _adminReplyDraft;
+            set
+            {
+                if (_adminReplyDraft == value)
+                {
+                    return;
+                }
+
+                _adminReplyDraft = value;
+                OnPropertyChanged(nameof(AdminReplyDraft));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
